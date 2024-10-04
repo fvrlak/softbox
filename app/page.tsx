86e641470a97f99be6afd8e2236303fd18934f6e
fvrlak <fvrlak@gmail.com>
2024-10-04 03:59:40 +0200

    init publish

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import Image from "next/image";
-// import { Dashboard } from "../components/dashboard/Dashboard";
 import Link from 'next/link';
 export default function Home() {
   return (
     <div>
-      {/* <Dashboard /> */}
       <h1>Hello World</h1>
       <Link href="/dashboard">Go to Dashboard</Link>
     </div>
